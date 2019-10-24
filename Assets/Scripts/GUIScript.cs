2f7a89b3dd6d27dc06b0efb2c2045384a739f4fb
Saša Barišić <cartman300@net.hr>
2019-10-24 05:58:03 +0200

    Added more assets

---
+++ Assets/Scripts/GUIScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUIScript : MonoBehaviour {
+	//public GameObject MenuPanel;
+
+	void Start() {
+
+
+	}
+
+	void CreateButton(string Name) {
+
+	}
+
+	void Update() {
+
+	}
+}
