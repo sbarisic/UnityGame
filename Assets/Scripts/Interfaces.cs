6108bc808b0e9603c5b88c33e5b95ed483c09749
Saša Barišić <cartman300@net.hr>
2019-10-25 19:47:42 +0200

    Latest

---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Scripts {
+	enum DamageType {
+		Default = 0,
+		Fire
+	}
+
+	interface IGameObject {
+	}
+
+	interface IDamagable  {
+		void DealDamage(int Damage, DamageType DType = DamageType.Default);
+	}
+
+	interface IAlive : IDamagable {
+		int GetHealth();
+
+		void Heal(int HP);
+	}
+}
---
+++ Assets/Utilities.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Game.Scripts {
+	static class Utilities {
+		public static MonoBehaviour GetScript(this Component C) {
+			Component[] Components = C.gameObject.GetComponents<MonoBehaviour>();
+
+			foreach (var Comp in Components)
+				if (Comp is IGameObject && Comp is MonoBehaviour B)
+					return B;
+
+			return null;
+		}
+	}
+}
