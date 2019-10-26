49cd6c449ead046fd675fedd04bdda761e93b20a
Saša Barišić <cartman300@net.hr>
2019-10-26 02:34:24 +0200

    Latest

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraScript : MonoBehaviour {
+	public Material PostProcessingMat;
+	public bool UsePostProcessing = true;
+
+	Camera Cam;
+
+	void Start() {
+		Cam = GetComponent<Camera>();
+		//Cam.backgroundColor = new Color(0, 0, 0, 0);
+	}
+
+	void OnRenderImage(RenderTexture Src, RenderTexture Dst) {
+		float W = Camera.current.pixelWidth;
+		float H = Camera.current.pixelHeight;
+
+		//float Ratio = W / H;
+		//float RatioW = Cam.orthographicSize * Ratio;
+		//Graphics.DrawTexture(new Rect(-(RatioW / 2), 0, RatioW, 1), Parallax1);
+
+		if (UsePostProcessing) {
+			PostProcessingMat.SetVector("Resolution", new Vector4(W, H, 0, 0));
+			Graphics.Blit(Src, Dst, PostProcessingMat);
+		} else
+			Graphics.Blit(Src, Dst);
+	}
+}
---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+	public Color Tint = new Color(0.4f, 0.4f, 0.4f);
+	public Vector2 Offset = new Vector2(0, 1.5f);
+	public Sprite[] BackgroundSprites;
+
+	SpriteRenderer[] Rnds;
+
+	void Start() {
+		if (BackgroundSprites == null)
+			return;
+
+		Rnds = new SpriteRenderer[BackgroundSprites.Length];
+
+		for (int i = 0; i < BackgroundSprites.Length; i++)
+			Rnds[i] = AddSpriteRenderer(-100 - i, BackgroundSprites[i], i >= BackgroundSprites.Length - 1 ? (Color?)null : Tint);
+
+		transform.position = new Vector3(Offset.x, Offset.y, 0);
+	}
+
+	SpriteRenderer AddSpriteRenderer(int Order, Sprite S, Color? Tint = null) {
+		GameObject RndObj = new GameObject();
+		RndObj.transform.parent = gameObject.transform;
+		RndObj.transform.localPosition = Vector3.zero;
+
+		SpriteRenderer Rnd = RndObj.AddComponent<SpriteRenderer>();
+		Rnd.sortingOrder = Order;
+		Rnd.sprite = S;
+
+		if (Tint != null)
+			Rnd.color = Tint.Value;
+
+		return Rnd;
+	}
+
+	void Update() {
+		Camera Cur = Camera.current;
+
+		if (Cur == null)
+			return;
+
+		Vector3 CamPos = Cur.transform.position;
+		transform.position = new Vector3(CamPos.x + Offset.x, CamPos.y + Offset.y, 0);
+	}
+}
