863492e1beb236293efd7d806ff430478128a5c3
Iva <ivavzivkovic@gmail.com>
2020-01-20 17:14:57 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+	public float speed = 7f;
+	public float minDistance;
+	public GameObject target;
+	public Vector3 offset;
+
+	private Vector3 targetPos;
+
+	// Use this for initialization
+	void Start() {
+		targetPos = transform.TransformPoint(offset);
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (target) {
+			Vector3 posNoZ = transform.position + offset;
+			Vector3 targetDirection = (target.transform.position - posNoZ);
+			float interpVelocity = targetDirection.magnitude * speed;
+			targetPos = (transform.position) + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+			transform.position = Vector3.Lerp(transform.position, targetPos, 0.25f);
+
+		}
+	}
+}
